6dc6f9539ca8f3d9ae317b9cc91f2e2e8825cc38
unknown <luccasdrapala@gmail.com>
2022-10-19 20:36:11 -0300

    configurando formulário front-end

--- includes/formulario.php
+++ includes/formulario.php
@@ -6,4 +6,38 @@
         </a>
     </section>
 
+    <h2 class="mt-2">Cadastrar vaga</h2>
+
+    <form action="" method="post">
+
+        <div class="form-group">
+            <label for="titulo">Titulo da Vaga</label>
+            <input type="text" name="titulo" id="titulo" class="form-control">
+        </div>
+
+        <div class="form-group mt-2">
+            <label for="descricao">Descrição</label>
+            <textarea name="descricao" id="descricao" class="form-control" rows="4"></textarea>
+        </div>
+
+        <div class="form-group mt-2">
+            <label for="status">Status</label>
+
+            <div class="form-check form-check-inline">
+                <label class="form-control">
+                    <input type="radio" name="ativo" value="s" checked>Ativo
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <label class="form-control">
+                    <input type="radio" name="ativo" value="n">Inativo
+                </label>
+            </div>
+        </div>
+
+        <div class="form-group mt-3">
+            <button class="btn btn-success" type="submit">Cadastrar</button>
+        </div>
+    </form>
 </main>
